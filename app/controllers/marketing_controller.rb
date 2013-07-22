5121f29738381500261bf769fa5caba5c89aaf45
Alekhya <alekhya.rudra@cybrilla.com>
2013-07-22 14:18:48 +0530

    initial commit

---
+++ app/controllers/marketing_controller.rb
@@ -0,0 +1,5 @@
+class MarketingController < ApplicationController
+
+	def what_is_scripbox
+	end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ABTest::Application.config.secret_token = '81b94932fc23cde4a39c84498cecc8c82cb91839a72282fe733b28e79ed2af06be5cc1955069330493c6a55361d7146662f3558b5a520dc77762897491f12bca'
